9261a8e50e7866293ad54f8ea4072864591cd2ef
AstroTheRabbit <66503946+AstroTheRabbit@users.noreply.github.com>
2024-12-04 23:23:37 +1100

    y2024 d4 - changed `Dir` enum to struct

--- src/y2024/d4.rs
+++ src/y2024/d4.rs
@@ -1,19 +1,30 @@
+use std::ops::Mul;
+
 const INPUT: &str = include_str!("../../inputs/y2024/d4.txt");
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-enum Dir {
-    PX,
-    PY,
-    NX,
-    NY,
-
-    PXPY,
-    PXNY,
-    NXPY,
-    NXNY,
+struct Dir {
+    pub x: isize,
+    pub y: isize,
 }
 
 impl Dir {
+    pub const fn new(x: isize, y: isize) -> Self {
+        Self { x, y }
+    }
+
+    pub const NONE: Self = Self::new(0, 0);
+
+    pub const PX: Self = Self::new(1, 0);
+    pub const PY: Self = Self::new(0, 1);
+    pub const NX: Self = Self::new(-1, 0);
+    pub const NY: Self = Self::new(0, -1);
+
+    pub const PXPY: Self = Self::new(1, 1);
+    pub const PXNY: Self = Self::new(1, -1);
+    pub const NXPY: Self = Self::new(-1, 1);
+    pub const NXNY: Self = Self::new(-1, -1);
+
     pub const ALL_DIRS: [Self; 8] = [
         Self::PX,
         Self::PY,
@@ -24,21 +35,13 @@ impl Dir {
         Self::NXPY,
         Self::NXNY,
     ];
+}
 
-    // * Returns bools matching (px, py, nx, ny).
-    #[rustfmt::skip]
-    pub fn get_checks(&self) -> (bool, bool, bool, bool) {
-        match self {
-            //            PX     PY     NX     NY
-            Dir::PX   => (true , false, false, false),
-            Dir::PY   => (false, true , false, false),
-            Dir::NX   => (false, false, true , false),
-            Dir::NY   => (false, false, false,  true),
-            Dir::PXPY => (true , true , false, false),
-            Dir::PXNY => (true , false, false,  true),
-            Dir::NXPY => (false, true , true , false),
-            Dir::NXNY => (false, false, true ,  true),
-        }
+impl Mul<Dir> for isize {
+    type Output = Dir;
+
+    fn mul(self, rhs: Dir) -> Self::Output {
+        return Dir::new(self * rhs.x, self * rhs.y);
     }
 }
 
@@ -53,34 +56,13 @@ impl Grid {
         }
     }
 
-    // * Get and return the char at (`x`, `y`) [optionally with the ], returning the new coordinate as well.
-    pub fn get_dir(
-        &self,
-        mut x: usize,
-        mut y: usize,
-        dir: Option<Dir>,
-    ) -> Option<(char, usize, usize)> {
-        let (px, py, nx, ny) = match dir {
-            Some(d) => d.get_checks(),
-            None => (false, false, false, false),
-        };
-        if (nx && x == 0) || (ny && y == 0) {
-            // ? Since this code only uses unsigned ints (and also because you can index a Vec below 0), this check is required.
-            return None;
-        }
-        if px {
-            x += 1;
-        } else if nx {
-            x -= 1;
-        }
-        if py {
-            y += 1;
-        } else if ny {
-            y -= 1;
-        }
-        if let Some(line) = self.chars.get(y) {
-            if let Some(&c) = line.get(x) {
-                return Some((c, x, y));
+    pub fn get_dir(&self, x: usize, y: usize, dir: Dir) -> Option<char> {
+        if let Some((x, y)) = Option::zip(x.checked_add_signed(dir.x), y.checked_add_signed(dir.y))
+        {
+            if let Some(line) = self.chars.get(y) {
+                if let Some(&c) = line.get(x) {
+                    return Some(c);
+                }
             }
         }
         return None;
@@ -91,32 +73,23 @@ pub fn solve_pt1() -> u32 {
     let grid = Grid::load();
     let mut res = 0;
 
-    for y0 in 0..grid.chars.len() {
-        for x0 in 0..grid.chars[y0].len() {
-            if grid.get_dir(x0, y0, None).is_none_or(|(c, ..)| c != 'X') {
-                // * character at (x0, y0) isn't an 'X'.
+    for y in 0..grid.chars.len() {
+        for x in 0..grid.chars[y].len() {
+            if grid.get_dir(x, y, Dir::NONE).is_none_or(|c| c != 'X') {
+                // * character at (x, y) isn't an 'X'.
                 continue;
             }
             for dir in Dir::ALL_DIRS {
-                let mut x = x0;
-                let mut y = y0;
-                let mut found = true;
-                for cc in ['M', 'A', 'S'] {
-                    // * Check each subsequent character in direction `dir` for the rest of "XMAS".
-                    match grid.get_dir(x, y, Some(dir)) {
-                        Some((c, new_x, new_y)) if c == cc => {
-                            x = new_x;
-                            y = new_y;
-                        }
-                        _ => {
-                            found = false;
-                            break;
-                        }
-                    }
+                if grid.get_dir(x, y, 1 * dir).is_none_or(|c| c != 'M') {
+                    continue;
                 }
-                if found {
-                    res += 1;
+                if grid.get_dir(x, y, 2 * dir).is_none_or(|c| c != 'A') {
+                    continue;
                 }
+                if grid.get_dir(x, y, 3 * dir).is_none_or(|c| c != 'S') {
+                    continue;
+                }
+                res += 1;
             }
         }
     }
@@ -129,24 +102,24 @@ pub fn solve_pt2() -> u32 {
 
     for y in 0..grid.chars.len() {
         for x in 0..grid.chars[y].len() {
-            if grid.get_dir(x, y, None).is_none_or(|(c, ..)| c != 'A') {
-                // * character at (x0, y0) cannot be the centre of an "X-MAS".
+            if grid.get_dir(x, y, Dir::NONE).is_none_or(|c| c != 'A') {
+                // * character at (x0, y0) cannot be the centre of an "X-MAS" pattern.
                 continue;
             }
-            let c_pxpy = match grid.get_dir(x, y, Some(Dir::PXPY)) {
-                Some((c, ..)) => c,
+            let c_pxpy = match grid.get_dir(x, y, Dir::PXPY) {
+                Some(c) => c,
                 None => continue,
             };
-            let c_pxny = match grid.get_dir(x, y, Some(Dir::PXNY)) {
-                Some((c, ..)) => c,
+            let c_pxny = match grid.get_dir(x, y, Dir::PXNY) {
+                Some(c) => c,
                 None => continue,
             };
-            let c_nxpy = match grid.get_dir(x, y, Some(Dir::NXPY)) {
-                Some((c, ..)) => c,
+            let c_nxpy = match grid.get_dir(x, y, Dir::NXPY) {
+                Some(c) => c,
                 None => continue,
             };
-            let c_nxny = match grid.get_dir(x, y, Some(Dir::NXNY)) {
-                Some((c, ..)) => c,
+            let c_nxny = match grid.get_dir(x, y, Dir::NXNY) {
+                Some(c) => c,
                 None => continue,
             };
 
@@ -156,15 +129,18 @@ pub fn solve_pt2() -> u32 {
             // * ....C.... => .A. | .A. | .A. | .A.
             // * NXPY.PXPY => M.S | S.M | S.S | M.M
 
-            match (c_nxny, c_pxny, c_nxpy, c_pxpy) {
-                ('M', 'S', 'M', 'S')
-                | ('S', 'M', 'S', 'M')
-                | ('M', 'M', 'S', 'S')
-                | ('S', 'S', 'M', 'M') => res += 1,
-                _ => continue,
+            let xmas_pattern = [c_nxny, c_pxny, c_nxpy, c_pxpy];
+            if [
+                ['M', 'S', 'M', 'S'],
+                ['S', 'M', 'S', 'M'],
+                ['M', 'M', 'S', 'S'],
+                ['S', 'S', 'M', 'M'],
+            ]
+            .contains(&xmas_pattern)
+            {
+                res += 1;
             }
         }
     }
-
     return res;
 }
