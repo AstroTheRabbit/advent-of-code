3691ef7819b0fb3db079b8ed586d54a547083397
AstroTheRabbit <66503946+AstroTheRabbit@users.noreply.github.com>
2024-12-11 17:28:12 +1100

    y2024 d11 - completed

--- src/main.rs
+++ src/main.rs
@@ -10,39 +10,41 @@ macro_rules! include_input {
 
 fn main() {
     println!("Hello, Advent of Code!");
-    // println!("2023 Solutions");
-    // println!("  day 1, part 1: {}", y2023::d1::solve_pt1());
-    // println!("  day 1, part 2: {}", y2023::d1::solve_pt2());
-    // println!("  day 2, part 1: {}", y2023::d2::solve_pt1());
-    // println!("  day 2, part 2: {}", y2023::d2::solve_pt2());
-    // println!("  day 3, part 1: {}", y2023::d3::solve_pt1());
-    // println!("  day 3, part 2: {}", y2023::d3::solve_pt2());
-    // println!("  day 4, part 1: {}", y2023::d4::solve_pt1());
-    // // println!("  day 4, part 2: {}", y2023::d4::solve_pt2()); // ! Slow
-    // println!("  day 5, part 1: {}", y2023::d5::solve_pt1());
-    // println!("  day 5, part 2: {}", y2023::d5::solve_pt2());
-    // println!("  day 6, part 1: {}", y2023::d6::solve_pt1());
-    // println!("  day 6, part 2: {}", y2023::d6::solve_pt2());
+    println!("2023 Solutions");
+    println!("  day 1, part 1: {}", y2023::d1::solve_pt1());
+    println!("  day 1, part 2: {}", y2023::d1::solve_pt2());
+    println!("  day 2, part 1: {}", y2023::d2::solve_pt1());
+    println!("  day 2, part 2: {}", y2023::d2::solve_pt2());
+    println!("  day 3, part 1: {}", y2023::d3::solve_pt1());
+    println!("  day 3, part 2: {}", y2023::d3::solve_pt2());
+    println!("  day 4, part 1: {}", y2023::d4::solve_pt1());
+    // println!("  day 4, part 2: {}", y2023::d4::solve_pt2()); // ! Slow
+    println!("  day 5, part 1: {}", y2023::d5::solve_pt1());
+    println!("  day 5, part 2: {}", y2023::d5::solve_pt2());
+    println!("  day 6, part 1: {}", y2023::d6::solve_pt1());
+    println!("  day 6, part 2: {}", y2023::d6::solve_pt2());
 
-    // println!("2024 Solutions");
-    // println!("  day 1, part 1: {}", y2024::d1::solve_pt1());
-    // println!("  day 1, part 2: {}", y2024::d1::solve_pt2());
-    // println!("  day 2, part 1: {}", y2024::d2::solve_pt1());
-    // println!("  day 2, part 2: {}", y2024::d2::solve_pt2());
-    // println!("  day 3, part 1: {}", y2024::d3::solve_pt1());
-    // println!("  day 3, part 2: {}", y2024::d3::solve_pt2());
-    // println!("  day 4, part 1: {}", y2024::d4::solve_pt1());
-    // println!("  day 4, part 2: {}", y2024::d4::solve_pt2());
-    // println!("  day 5, part 1: {}", y2024::d5::solve_pt1());
-    // println!("  day 5, part 2: {}", y2024::d5::solve_pt2());
-    // println!("  day 6, part 1: {}", y2024::d6::solve_pt1());
-    // // println!("  day 6, part 2: {}", y2024::d6::solve_pt2()); // ! Slow
-    // // println!("  day 7, part 1: {}", y2024::d7::solve_pt1()); // ! Slow
-    // // println!("  day 7, part 2: {}", y2024::d7::solve_pt2()); // ! Slow
-    // println!("  day 8, part 1: {}", y2024::d8::solve_pt1());
-    // println!("  day 8, part 2: {}", y2024::d8::solve_pt2());
-    // println!("  day 9, part 1: {}", y2024::d9::solve_pt1());
-    // println!("  day 9, part 2: {}", y2024::d9::solve_pt2());
+    println!("2024 Solutions");
+    println!("  day 1, part 1: {}", y2024::d1::solve_pt1());
+    println!("  day 1, part 2: {}", y2024::d1::solve_pt2());
+    println!("  day 2, part 1: {}", y2024::d2::solve_pt1());
+    println!("  day 2, part 2: {}", y2024::d2::solve_pt2());
+    println!("  day 3, part 1: {}", y2024::d3::solve_pt1());
+    println!("  day 3, part 2: {}", y2024::d3::solve_pt2());
+    println!("  day 4, part 1: {}", y2024::d4::solve_pt1());
+    println!("  day 4, part 2: {}", y2024::d4::solve_pt2());
+    println!("  day 5, part 1: {}", y2024::d5::solve_pt1());
+    println!("  day 5, part 2: {}", y2024::d5::solve_pt2());
+    println!("  day 6, part 1: {}", y2024::d6::solve_pt1());
+    // println!("  day 6, part 2: {}", y2024::d6::solve_pt2()); // ! Slow
+    // println!("  day 7, part 1: {}", y2024::d7::solve_pt1()); // ! Slow
+    // println!("  day 7, part 2: {}", y2024::d7::solve_pt2()); // ! Slow
+    println!("  day 8, part 1: {}", y2024::d8::solve_pt1());
+    println!("  day 8, part 2: {}", y2024::d8::solve_pt2());
+    println!("  day 9, part 1: {}", y2024::d9::solve_pt1());
+    println!("  day 9, part 2: {}", y2024::d9::solve_pt2());
     println!("  day 10, part 1: {}", y2024::d10::solve_pt1());
     println!("  day 10, part 2: {}", y2024::d10::solve_pt2());
+    println!("  day 11, part 1: {}", y2024::d11::solve_pt1());
+    println!("  day 11, part 2: {}", y2024::d11::solve_pt2());
 }
---
+++ src/y2024/d11.rs
@@ -0,0 +1,83 @@
+use std::collections::HashMap;
+
+use crate::include_input;
+
+const INPUT: &str = include_input!("2024", "11");
+
+/// Add `count` to the value of `map[key]`, or insert it if not found.
+fn add_or_insert(map: &mut HashMap<u64, u64>, key: u64, count: u64) {
+    match map.get_mut(&key) {
+        Some(value) => {
+            *value += count;
+        },
+        None => {
+            map.insert(key, count);
+        },
+    }
+}
+
+fn load_stones() -> HashMap<u64, u64> {
+    return INPUT.split(' ').map(|n| (n.parse().unwrap(), 1)).collect();
+}
+
+pub fn solve_pt1() -> u64 {
+    const NUM_BLINKS: u64 = 25;
+    let mut stones = load_stones();
+    let mut next = HashMap::new();
+
+    for _ in 0..NUM_BLINKS {
+        for (value, count) in stones.drain() {
+            if value == 0 {
+                // * Rule 1
+                add_or_insert(&mut next, 1, count);
+                continue;
+            }
+            let as_str = value.to_string();
+            let str_len = as_str.len();
+            if str_len % 2 == 0 {
+                // * Rule 2
+                let (first, second) = as_str.split_at(str_len / 2);
+                let first = first.parse().unwrap();
+                let second = second.parse().unwrap();
+                add_or_insert(&mut next, first, count);
+                add_or_insert(&mut next, second, count);
+            } else {
+                // * Rule 3
+                add_or_insert(&mut next, 2024 * value, count);
+            }
+        }
+        std::mem::swap(&mut stones, &mut next);
+    }
+    return stones.into_values().sum();
+}
+
+pub fn solve_pt2() -> u64 {
+    const NUM_BLINKS: u64 = 75;
+    let mut stones = load_stones();
+    let mut next = HashMap::new();
+
+    for _ in 0..NUM_BLINKS {
+        for (value, count) in stones.drain() {
+            if value == 0 {
+                // * Rule 1
+                add_or_insert(&mut next, 1, count);
+                continue;
+            }
+            let as_str = value.to_string();
+            let str_len = as_str.len();
+            if str_len % 2 == 0 {
+                // * Rule 2
+                let (first, second) = as_str.split_at(str_len / 2);
+                let first = first.parse().unwrap();
+                let second = second.parse().unwrap();
+                add_or_insert(&mut next, first, count);
+                add_or_insert(&mut next, second, count);
+            } else {
+                // * Rule 3
+                add_or_insert(&mut next, 2024 * value, count);
+            }
+        }
+        std::mem::swap(&mut stones, &mut next);
+    }
+    return stones.into_values().sum();
+}
